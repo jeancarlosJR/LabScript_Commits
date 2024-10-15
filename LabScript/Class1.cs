90bc5764cab1d60f983a5f9bd0dafa397b9b67f5
Jean Quiñones <38512817@claro.com.co>
2024-10-15 10:42:22 -0500

    commit 1

---
+++ LabScript/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.Serialization;
+
+using Microsoft.JSInterop;
+
+namespace LabScript.Pages
+{
+    public class AlertService : IAsyncDisposable, IAlertService
+    {
+
+        readonly Lazy<Task<IJSObjectReference>> ijsObjectReference;
+
+        public AlertService(IJSRuntime ijsRuntime)
+        {
+            this.ijsObjectReference = new Lazy<Task<IJSObjectReference>>(() =>
+            ijsRuntime.InvokeAsync<IJSObjectReference>("import",
+            "./Home.js").AsTask());
+
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (ijsObjectReference.IsValueCreated)
+            {
+                IJSObjectReference moduleJs = await ijsObjectReference.Value;
+                await moduleJs.DisposeAsync();
+            }
+        }
+
+        public async Task CallJsAlertFunction()
+        {
+            var jsModule = await ijsObjectReference.Value;
+
+            await jsModule.InvokeVoidAsync("jsFuncion");
+        }
+    }
+}
+
+
+
